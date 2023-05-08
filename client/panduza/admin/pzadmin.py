124e6179174656be140faa81a68aa6a7b0b56bf3
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2023-05-08 08:22:14 +0200

    Improve pzadmin (#54)
    * append the setup fonction to install docker
    
    * houlala ca commence a etre bien
    
    * append the scan command to pzadmin
    
    ---------
    
    Co-authored-by: Rodriguez <xavier.rodriguez@advans-group.com>

--- client/panduza/admin/pzadmin.py
+++ client/panduza/admin/pzadmin.py
@@ -3,6 +3,9 @@ import argparse
 
 from .hunt import hunt
 from .start import start
+from .stop import stop
+from .setup import setup
+from .scan import scan
 from .init_directory import init_directory
 
 
@@ -10,29 +13,34 @@ from .init_directory import init_directory
 def pzadmin_main():
 
     parser = argparse.ArgumentParser(description='Panduza Admin Helper')
+    parser.add_argument('command', metavar='command', type=str,
+                        choices=["init", "hunt", "start", "stop", "setup", "scan"],
+                        help='command to execute')
     parser.add_argument('directory_path', metavar='directory_path', type=str,
-                        default='.',
+                        default='.', nargs='?',
                         help='path to the directory')
-    parser.add_argument('--init', dest='init', action='store_true',
-                        help='initialize the directory')
-    parser.add_argument('--hunt', dest='hunt', action='store_true',
-                        help='start hunting in the directory')
-    parser.add_argument('--start', dest='start', action='store_true',
-                        help='start processing in the directory')
-    parser.add_argument('--stop', dest='stop', action='store_true',
-                        help='stop processing in the directory')
+    parser.add_argument('-d', dest='deamon', action='store_true',
+                        help='for the command "start", the platform is started in background')
+    parser.add_argument('--stop-all', dest='stopall', action='store_true',
+                        help='for the command "stop", mosquitto is also stopped')
 
     args = parser.parse_args()
 
-    print(f'Working directory: {args.directory_path}')
+    # print(f'Command             : {args.command}')
+    # print(f'Working directory   : {args.directory_path}')
 
-    if args.init:
-        init_directory(args.directory_path)
-    elif args.hunt:
+    if args.command == "init":
+        init_directory(args)
+    elif args.command == "hunt":
         hunt(args.directory_path)
-    elif args.start:
-        start(args.directory_path)
-    elif args.stop:
-        print(f'Stopping to process directory: {args.directory_path}')
+    elif args.command == "start":
+        start(args)
+    elif args.command == "stop":
+        stop(args)
+    elif args.command == "setup":
+        setup(args)
+    elif args.command == "scan":
+        scan(args)
     else:
         print(f'Invalid arguments provided for pzadmin.')
+
