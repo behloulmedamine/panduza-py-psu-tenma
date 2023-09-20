48b7fcde774ed700d8279b2ea278e85472afcdb9
behloulmedamine <behloulamine30@gmail.com>
2023-09-20 10:40:58 +0200

    Add tests

---
+++ tests/py_tests/test2.py
@@ -0,0 +1,16 @@
+import time
+from panduza import Bpc
+
+ADDR="localhost"
+PORT=1883
+
+# power_channel = Bpc(addr=ADDR, port=PORT, topic="pza/default/Hanmatek_Hm310t/bpc")
+# power_channel = Bpc(addr=ADDR, port=PORT, topic="pza/default/Panduza_FakeBps/channel_1")
+power_channel = Bpc(addr=ADDR, port=PORT, topic="pza/default/Tenma_722710/bpc")
+
+#power_channel.voltage.value.set(6.0)
+#power_channel.current.value.set(1.4)
+
+power_channel.enable.value.set(True)
+#time.sleep(5)
+#power_channel.enable.value.set(False)
